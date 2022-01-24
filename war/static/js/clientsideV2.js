b4d46aa3ec2e7537fdad65fa08d441f903ae8b47
pys890102 <pys890102@bkwinnners.com>
2022-01-24 16:36:40 +0900

    live로 변경

--- war/static/js/clientsideV2.js
+++ war/static/js/clientsideV2.js
@@ -15,7 +15,8 @@ var MARU = (function (win, doc) {
   /* GLOBAL */
   var routeUrls = {
     sandbox: 'https://devapi.bkwinners.kr',
-    live: 'https://api.bkwinners.kr'
+//    live: 'https://api.bkwinners.kr'
+	live: 'https://devapi.bkwinners.kr'
   }
   var routeDomain = routeUrls[c3Config.debugMode];
   var layerInited = false;            // 레이어 생성이 완료되었는지
