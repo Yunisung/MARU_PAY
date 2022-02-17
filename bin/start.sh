fea182a1c46e06af50c39152fe5433a68b0ae5f4
pys890102 <pys890102@bkwinnners.com>
2022-02-17 16:48:40 +0900

    webroot원복

--- bin/start.sh
+++ bin/start.sh
@@ -21,6 +21,6 @@ JVM_ARGS="$JVM_ARGS -Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2 -Djdk.tls.r
 JVM_ARGS="$JVM_ARGS -Djdk.tls.ephemeralDHKeySize=2048 "
 JVM_ARGS="$JVM_ARGS -Dorg.vertx.logger-delegate-factory-class-name=org.vertx.java.core.logging.impl.SLF4JLogDelegateFactory "
 JVM_ARGS="$JVM_ARGS -Xss512k -Xms128m -Xmx256m "
-JVM_ARGS="$JVM_ARGS -cp $CP"
+
 java $JVM_ARGS com.pgmate.lib.vertx.main.VertXServer &
 echo $!>apio.pid
