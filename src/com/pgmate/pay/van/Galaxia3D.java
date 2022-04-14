bddf4a08ffdcfba534de570f1a7c90a6eb6b2c42
kjm <jjggooo>
2022-04-14 15:00:24 +0900

    ssl 설정 수정2
    (cherry picked from commit 1754af4375acfc111e80248515ef12485c1a3b98)

--- src/com/pgmate/pay/van/Galaxia3D.java
+++ src/com/pgmate/pay/van/Galaxia3D.java
@@ -55,7 +55,7 @@ public class Galaxia3D {
 				}
 			});
 			
-			SSLContext context = SSLContext.getInstance("TLSv1.2");
+			SSLContext context = SSLContext.getInstance("TLS");
 			context.init(null, null, null);
 			conn.setSSLSocketFactory(context.getSocketFactory());
 			
