fbf06f56ac8951b1b959f2cc3c7d1fdb0f9f94a9
pys890102 <pys890102@bkwinnners.com>
2022-05-30 16:33:06 +0900

    url 변경

--- war/static/js/clientside.js
+++ war/static/js/clientside.js
@@ -349,7 +349,7 @@ var MARU = (function (win, doc) {
 
   function echo(key, success, fail) {
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-    xhr.open('GET', '/api/echo');
+    xhr.open('GET', routeDomain + '/api/echo');
     xhr.onreadystatechange = function() {
         if (xhr.readyState > 3 && xhr.status == 200) {
             //console.log(JSON.parse(xhr.responseText));
