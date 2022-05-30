eb67ca0d5335b49d75df03e86e6f9b3a2589003d
pys890102 <pys890102@bkwinnners.com>
2022-05-30 15:34:40 +0900

    clientside 수정
    (cherry picked from commit 7d6a03e35fc07314fb203629d24968d1c8628b0c)
    (cherry picked from commit b513390d02ce0580032979071049a3e501341ae9)

--- war/static/js/clientside.js
+++ war/static/js/clientside.js
@@ -348,15 +348,22 @@ var MARU = (function (win, doc) {
   }
 
   function echo(key, success, fail) {
-    c3pop();
-    paykey = key;
-    echoSuccess = success;
-    echoFail = fail;
-
-    console.log(paykey);
-    setTimeout(function() {
-      postMessages.echo();
-    }, 200);
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xhr.open('GET', '/api/echo');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState > 3 && xhr.status == 200) {
+            //console.log(JSON.parse(xhr.responseText));
+            var res = JSON.parse(xhr.responseText);
+            success(res);
+        } else {
+          fail();
+        }
+    };
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Accept-Language", "ko_KR");
+    xhr.setRequestHeader("Authorization", key);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
   }
 
   function setDebug(bool) {
