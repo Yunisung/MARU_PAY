27a886185f32fa9de590d87bb985859de320e18f
pys890102 <pys890102@bkwinnners.com>
2022-06-30 10:57:03 +0900

    온라인 결제 테스트 2

--- src/com/pgmate/pay/proc/ProcPay3DV2Widget.java
+++ src/com/pgmate/pay/proc/ProcPay3DV2Widget.java
@@ -351,7 +351,7 @@ public class ProcPay3DV2Widget extends Proc {
 			form.put("sndInstallmenttype", installment);
 		}
 		
-		form.put("sndShowcard", "C(01:02:03:04:05:08:09)")
+		form.put("sndShowcard", "C(01:02:03:04:05:08:09)");
 		
 		form.put("reWHCid", "");				//승인 후 수취 필드
 		form.put("reWHCtype", "");				//승인 후 수취 필드
