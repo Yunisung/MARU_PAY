785b28ec7a9e6135109a8330ccb0f73b4dd4acf6
pys890102 <pys890102@bkwinnners.com>
2022-07-24 17:34:47 +0900

    카카오페이 PC 개발완료
    (cherry picked from commit 08ddea2a4ced6d50cdb87d0c4d0283898b8c1343)

--- src/com/pgmate/pay/util/TemplateUtil.java
+++ src/com/pgmate/pay/util/TemplateUtil.java
@@ -1,7 +1,9 @@
 package com.pgmate.pay.util;
 
+import com.pgmate.pay.bean.SimplePayResult;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.ext.web.RoutingContext;
+
 
 public class TemplateUtil {
 	//KJM : 온라인 결제 결과
@@ -239,5 +241,101 @@ public class TemplateUtil {
 		.putHeader(HttpHeaders.CONNECTION, "close")
 		.putHeader(HttpHeaders.SERVER, "CREDITOP")
 		.write(sb.toString()).end();
+	}
+
+	public static void kakaoPayResultPage(RoutingContext rc, SimplePayResult kakaoRes) {
+		String returnPage =
+				"<html>\n" +
+				"<head>\n" +
+				"<title>KSPay</title>\n" +
+				"<meta http-equiv=\"Content-Type\" content=\"text/html charset=euc-kr\">\n" +
+				"<style type=\"text/css\">\n" +
+				"\tTABLE{font-size:9pt; line-height:160%;}\n" +
+				"\tA {color:blueline-height:160% background-color:#E0EFFE}\n" +
+				"\tINPUT{font-size:9pt}\n" +
+				"\tSELECT{font-size:9pt}\n" +
+				"\t.emp{background-color:#FDEAFE}\n" +
+				"\t.white{background-color:#FFFFFF color:black border:1x solid white font-size: 9pt}\n" +
+				"</style>\n" +
+				"</head>"+
+				"<table border=0 width=0>\n" +
+				"<tr>\n" +
+				"<td align=center>\n" +
+				"<table width=320 cellspacing=0 cellpadding=0 border=0 bgcolor=#4F9AFF>\n" +
+				"<tr>\n" +
+				"<td>\n" +
+				"<table width=100% cellspacing=1 cellpadding=2 border=0>\n" +
+				"<tr bgcolor=#4F9AFF height=25>\n" +
+				"<td align=left><font color=\"#FFFFFF\">\n" +
+				"카카오페이 결과</td>\n" +
+				"</tr>\n" +
+				"<tr bgcolor=#FFFFFF>\n" +
+				"<td valign=top>\n" +
+				"<table width=100% cellspacing=0 cellpadding=2 border=0>\n" +
+				"<tr>\n" +
+				"<td align=left>\n" +
+				"<table>\n" +
+				"<tr>\n" +
+				"\t<td>거래종류 :</td>\n" +
+				"\t<td>"+kakaoRes.rApprovalType +"</td>\n" +
+				"</tr>\n" +
+				"<tr>\n" +
+				"\t<td>거래번호 :</td>\n" +
+				"\t<td>"+kakaoRes.rTransactionNo +"</td>\n" +
+				"</tr>\n" +
+				"<tr>\n" +
+				"\t<td>거래성공여부 :</td>\n" +
+				"\t<td>"+kakaoRes.rStatus+"</td>\n" +
+				"</tr>\n" +
+				"<tr>\n" +
+				"\t<td>거래시간 :</td>\n" +
+				"\t<td>"+kakaoRes.rTradeDate+"&nbsp;"+kakaoRes.rTradeTime+"</td>\n" +
+				"</tr>\n" +
+				"<tr>\n" +
+				"\t<td>발급사코드 :</td>\n" +
+				"\t<td>"+kakaoRes.rIssCode+"</td>\n" +
+				"</tr>\n" +
+				"<tr>\n" +
+				"\t<td>매입사코드 :</td>\n" +
+				"\t<td>"+kakaoRes.rAquCode+"</td>\n" +
+				"</tr>\n" +
+				"<tr>\n" +
+				"\t<td>승인번호 :</td>\n" +
+				"\t<td>"+kakaoRes.rAuthNo+"</td>\n" +
+				"</tr>\n" +
+				"<tr>\n" +
+				"\t<td>메시지1 :</td>\n" +
+				"\t<td>"+kakaoRes.rMessage1+"</td>\n" +
+				"</tr>\n" +
+				"<tr>\n" +
+				"\t<td>메시지2 :</td>\n" +
+				"\t<td>"+kakaoRes.rMessage2+"</td>\n" +
+				"</tr>\n" +
+				"</table>\n" +
+				"</tr>\n" +
+				"</table>\n" +
+				"</td>\n" +
+				"</tr>\n" +
+				"</table>\n" +
+				"</td>\n" +
+				"</tr>\n" +
+				"</table>\n" +
+				"</td>\n" +
+				"</tr>\n" +
+				"</table>\n" +
+				"</td>\n" +
+				"</tr>\n" +
+				"</table>\n" +
+				"</table>" +
+				"</body>\n" +
+				"</html>";
+
+		rc.response().putHeader(HttpHeaders.CONTENT_TYPE, "text/html; charset=UTF-8")
+		.putHeader(HttpHeaders.CONTENT_LENGTH, ""+returnPage.toString().getBytes().length)
+		.putHeader(HttpHeaders.CACHE_CONTROL, "no-store")
+		.putHeader(HttpHeaders.EXPIRES, "-1")
+		.putHeader(HttpHeaders.CONNECTION, "close")
+		.putHeader(HttpHeaders.SERVER, "CREDITOP")
+		.write(returnPage.toString()).end();
 	}
 }
