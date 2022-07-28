ac541c93804baa42ba282e7d1f3c8e8a67d875f3
kjm <jjggooo>
2022-07-28 16:35:20 +0900

    임시 커밋

--- src/com/pgmate/pay/proc/ProcPay3DV2Widget.java
+++ src/com/pgmate/pay/proc/ProcPay3DV2Widget.java
@@ -654,10 +654,10 @@ public class ProcPay3DV2Widget extends Proc {
 			}
 		}else{
 			if(request.widget.isEquals("device", "mobile")) {
-				form.put("sndReply", String.format("https://%s%s/%s",PAYUNIT.PAY_HOST_LIVE,PAYUNIT.API_PAYCO_MOBILE_RETURN,sharedMap.getString(PAYUNIT.TRX_ID)));
+				form.put("sndReply", String.format("https://%s%s/%s",PAYUNIT.PAY_HOST_DEV,PAYUNIT.API_PAYCO_MOBILE_RETURN,sharedMap.getString(PAYUNIT.TRX_ID)));
 				form.put("orderChannel", "MOBILE");
 			} else {
-				form.put("sndReply", String.format("https://%s%s/%s", PAYUNIT.PAY_HOST_LIVE, PAYUNIT.API_PAYCO_RETURN, sharedMap.getString(PAYUNIT.TRX_ID)));
+				form.put("sndReply", String.format("https://%s%s/%s", PAYUNIT.PAY_HOST_DEV, PAYUNIT.API_PAYCO_RETURN, sharedMap.getString(PAYUNIT.TRX_ID)));
 			}
 		}
 
