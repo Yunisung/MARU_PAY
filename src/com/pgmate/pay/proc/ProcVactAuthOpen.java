788295bd22f33472460482db00870e7aa05bd510
kjm <jjggooo>
2022-11-09 16:31:19 +0900

    vact_dtl 히스토리 추가 merge

--- src/com/pgmate/pay/proc/ProcVactAuthOpen.java
+++ src/com/pgmate/pay/proc/ProcVactAuthOpen.java
@@ -160,8 +160,12 @@ public class ProcVactAuthOpen extends Proc{
             boolean execute = false;
             if(vact.isEquals("vactType","임시")){
                 execute = trxDAO.insertVactDtl(vact);
+                //HT_VACT_DTL insert
+                trxDAO.insertHtVactDtl(issueId, response.result.resultCd, response.result.resultMsg);
             }else{
                 execute = trxDAO.updateVactDtl(vact);
+                //HT_VACT_DTL insert
+                trxDAO.insertHtVactDtl(issueId, response.result.resultCd, response.result.resultMsg);
             }
 
             if(execute){
@@ -450,6 +454,8 @@ public class ProcVactAuthOpen extends Proc{
             logger.info("예금주 실명조회 오류 [{}][{}][{}][{}][{}][{}]", account, withdrawBankCd, withdrawAccount, identity, firmBean.resultCd, firmBean.resultMsg);
             return false;
         } else {
+            response.result.resultCd = firmBean.resultCd;
+            response.result.resultMsg = firmBean.resultMsg;
             /*if(!request.vact.holderName.trim().equals(firmBean.data.getString("customerName"))) {
                 logger.info("API인증 예금주 실명조회 비교오류 [{}][{}][{}][{}][{}]", request.vact.authBankCd, request.vact.authAccount, request.vact.identity, request.vact.holderName.trim(), firmBean.data.getString("name"));
 
@@ -611,6 +617,7 @@ public class ProcVactAuthOpen extends Proc{
             //PG_VACT_REG
             trxDAO.insertVactReg(mchtMap.getString("mchtId"), request.vact.bankCd, request.vact.account, request.auth.bankCd, request.auth.account,
                     request.vact.holderName, request.vact.trackId, request.vact.udf1, request.vact.udf2);
+
 
             //통합인증 수수료계산
             fee = mchtVactMngMap.getLong("totalAuthFee");
