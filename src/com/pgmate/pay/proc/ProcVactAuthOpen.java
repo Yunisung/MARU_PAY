1343777fb0019cb95bd9329235f73453e8fddd68
kjm <jjggooo>
2022-11-10 17:04:44 +0900

    comm > host 정보 주석

--- src/com/pgmate/pay/proc/ProcVactAuthOpen.java
+++ src/com/pgmate/pay/proc/ProcVactAuthOpen.java
@@ -427,8 +427,6 @@ public class ProcVactAuthOpen extends Proc{
         String trxType = request.vact.trxType;
         String type = "등록";
 
-
-
 //        if("2".equals(trxType)) {
 //            type = "변경";
 //        }
@@ -514,10 +512,10 @@ public class ProcVactAuthOpen extends Proc{
         long time = System.currentTimeMillis();
 
         try{
-//            socket = new Socket(host, port);
-//            socket.setSoTimeout(timeout);
-            socket = new Socket("10.100.200.10", 10006);
-            socket.setSoTimeout(70000);
+            socket = new Socket(host, port);
+            socket.setSoTimeout(timeout);
+//            socket = new Socket("10.100.200.10", 10006);
+//            socket.setSoTimeout(70000);
 
             output = socket.getOutputStream();
 //            output.write(reqJson.getBytes(Charset.forName("MS949")));
