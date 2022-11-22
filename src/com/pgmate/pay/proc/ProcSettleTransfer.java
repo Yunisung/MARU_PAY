5b10c7205ecf139d0c2adbe31d6413f1a8668dcb
pys890102 <pys890102@bkwinners.com>
2022-11-22 16:43:24 +0900

    출금API - IP체크 추가

--- src/com/pgmate/pay/proc/ProcSettleTransfer.java
+++ src/com/pgmate/pay/proc/ProcSettleTransfer.java
@@ -7,6 +7,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CodingErrorAction;
 
+import com.pgmate.lib.vertx.main.VertXUtil;
 import com.pgmate.pay.bean.Transfer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -189,6 +190,21 @@ public class ProcSettleTransfer extends Proc {
 		
 		if(request.transfer.amount < 1){
 			response.result = ResultUtil.getResult("9999", "이체 최소 금액 오류","이체금액은 1원 이상만 가능합니다.");return;
+		}
+
+		//PYS : IP체크로직 추가
+		//3.38.6.59 		: 에이블 라이브서버
+		//15.164.142.118 	: 에이블 개발서버
+		boolean ipChecker = true;
+		String clientIp = VertXUtil.getClientIp(rc);
+
+		if(clientIp.indexOf("15.164.142.118") > -1 || clientIp.indexOf("3.38.6.59") > -1 ||
+				clientIp.indexOf("192.168.") > -1 || clientIp.indexOf("10.100.100.") > -1){
+			ipChecker = false;
+		}
+
+		if(ipChecker == true) {
+			response.result = ResultUtil.getResult("9999", "허용된 IP 아님","접근 가능한 IP가 아닙니다.");return;
 		}
 		
 
