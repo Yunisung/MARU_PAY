c891ebab5462b539743f8dd8eb801c7854eb5f5b
pys890102 <pys890102@bkwinners.com>
2023-01-02 17:40:02 +0900

    터미널 조회 API 추가

---
+++ src/com/pgmate/pay/bean/Tmn.java
@@ -0,0 +1,11 @@
+package com.pgmate.pay.bean;
+
+public class Tmn {
+
+    public Tmn() {
+    }
+
+    public String van           = null; //VAN이름
+    public String semiAuth      = null; //비생인증유무
+
+}
--- src/com/pgmate/pay/main/Api.java
+++ src/com/pgmate/pay/main/Api.java
@@ -173,6 +173,8 @@ public class Api {
 				process = new ProcVactAuthOpen();
 			}else if (uri.startsWith(PAYUNIT.API_VACT_WITHDRAW_TRMN)) {
 				process = new ProcWithdrawTrmn();	// 가상계좌발급해지
+			}else if (uri.startsWith(PAYUNIT.API_TMN)) {
+				process = new ProcTmn();
 			}
 			else {
 				if (uri.startsWith(PAYUNIT.API_WEBHOOK_DANAL)) {
