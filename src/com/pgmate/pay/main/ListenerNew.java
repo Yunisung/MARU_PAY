c741fc3bbaaa02105542eb5e97ed0ed7745a7bc1
pys890102 <pys890102@bkwinners.com>
2023-02-23 13:09:25 +0900

    통합인증 : 멀티쓰레드 적용

--- src/com/pgmate/pay/main/ListenerNew.java
+++ src/com/pgmate/pay/main/ListenerNew.java
@@ -133,7 +133,7 @@ public class ListenerNew extends RouteWorkerNew {
 						VertXMessage.set500(fc);
 					}
 				});
-		
+
 		router.route(PAYUNIT.API_ARSV2_CHECK)
 				.handler(rc -> {
 					// poolSize defualt : 20
@@ -162,6 +162,105 @@ public class ListenerNew extends RouteWorkerNew {
 						logger.debug("{} not found ", fc.request().uri());
 					} else {
 						logger.error("{} error : {},{}", PAYUNIT.API_ARSV2_AUTH, fc.statusCode(), CommonUtil.getExceptionMessage(new Exception(fc.failure())));
+						VertXMessage.set500(fc);
+					}
+				});
+
+		//230223_PYS : 통합인증 멀티쓰레드 적용.
+		router.route(PAYUNIT.API_ACCOUNT_HOLDER)
+				.handler(rc -> {
+					// poolSize defualt : 20
+					int poolSize = 100;
+					//long maxExecueTime = 120 *1000; // 2분
+					long maxExecueTime = 5 * 60 *1000; // 5분
+					WorkerExecutor executor = vertx.createSharedWorkerExecutor("API_ACCOUNT_HOLDER", poolSize, maxExecueTime);
+					executor.executeBlocking(future -> {
+						log(rc);
+						logger.info("API_ACCOUNT_HOLDER START!!!");
+
+						new Api().apiHandler(rc);
+
+						future.complete();
+					}, false, res ->{
+
+						//new Api().apiHandler(rc);
+
+						executor.close();
+						//logger.info("API_ARS_AUTH_ASYNC RESULT[{}}]", res.result());
+						logger.info("API_ACCOUNT_HOLDER END!!!");
+					});
+				})
+				.failureHandler(fc -> {
+					if (fc.statusCode() == 404) {
+						logger.debug("{} not found ", fc.request().uri());
+					} else {
+						logger.error("{} error : {},{}", PAYUNIT.API_ACCOUNT_HOLDER, fc.statusCode(), CommonUtil.getExceptionMessage(new Exception(fc.failure())));
+						VertXMessage.set500(fc);
+					}
+				});
+
+		//230223_PYS : 통합인증 멀티쓰레드 적용.
+		router.route(PAYUNIT.API_ACCOUNT_TRANSFER)
+				.handler(rc -> {
+					// poolSize defualt : 20
+					int poolSize = 100;
+					//long maxExecueTime = 120 *1000; // 2분
+					long maxExecueTime = 5 * 60 *1000; // 5분
+					WorkerExecutor executor = vertx.createSharedWorkerExecutor("API_ACCOUNT_TRANSFER", poolSize, maxExecueTime);
+					executor.executeBlocking(future -> {
+						log(rc);
+						logger.info("API_ACCOUNT_TRANSFER START!!!");
+
+						new Api().apiHandler(rc);
+
+						future.complete();
+					}, false, res ->{
+
+						//new Api().apiHandler(rc);
+
+						executor.close();
+						//logger.info("API_ARS_AUTH_ASYNC RESULT[{}}]", res.result());
+						logger.info("API_ACCOUNT_TRANSFER END!!!");
+					});
+				})
+				.failureHandler(fc -> {
+					if (fc.statusCode() == 404) {
+						logger.debug("{} not found ", fc.request().uri());
+					} else {
+						logger.error("{} error : {},{}", PAYUNIT.API_ACCOUNT_TRANSFER, fc.statusCode(), CommonUtil.getExceptionMessage(new Exception(fc.failure())));
+						VertXMessage.set500(fc);
+					}
+				});
+
+		//230223_PYS : 통합인증 멀티쓰레드 적용.
+		router.route(PAYUNIT.API_ACCOUNT_AUTH_CHECK)
+				.handler(rc -> {
+					// poolSize defualt : 20
+					int poolSize = 100;
+					//long maxExecueTime = 120 *1000; // 2분
+					long maxExecueTime = 5 * 60 *1000; // 5분
+					WorkerExecutor executor = vertx.createSharedWorkerExecutor("API_ACCOUNT_AUTH_CHECK", poolSize, maxExecueTime);
+					executor.executeBlocking(future -> {
+						log(rc);
+						logger.info("API_ACCOUNT_AUTH_CHECK START!!!");
+
+						new Api().apiHandler(rc);
+
+						future.complete();
+					}, false, res ->{
+
+						//new Api().apiHandler(rc);
+
+						executor.close();
+						//logger.info("API_ARS_AUTH_ASYNC RESULT[{}}]", res.result());
+						logger.info("API_ACCOUNT_AUTH_CHECK END!!!");
+					});
+				})
+				.failureHandler(fc -> {
+					if (fc.statusCode() == 404) {
+						logger.debug("{} not found ", fc.request().uri());
+					} else {
+						logger.error("{} error : {},{}", PAYUNIT.API_ACCOUNT_AUTH_CHECK, fc.statusCode(), CommonUtil.getExceptionMessage(new Exception(fc.failure())));
 						VertXMessage.set500(fc);
 					}
 				});
