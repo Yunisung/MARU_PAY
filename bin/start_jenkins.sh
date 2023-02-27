1e37c88761ab8576a54f867eadb5fb9cb75a72ca
yangwenry <yangwenry@bkwinners.com>
2023-02-27 17:48:56 +0900

    WAS1 임시로 최대메모리 512m 변경 - 롤백

--- bin/start_jenkins.sh
+++ bin/start_jenkins.sh
@@ -21,7 +21,7 @@ JVM_ARGS="-DMARU_PAY -server -DCP_CONF=$MARU_PATH/conf -Dlogback.configurationFi
 JVM_ARGS="$JVM_ARGS -Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2 -Djdk.tls.rejectClientInitiatedRenegotiation=true "
 JVM_ARGS="$JVM_ARGS -Djdk.tls.ephemeralDHKeySize=2048 "
 JVM_ARGS="$JVM_ARGS -Dorg.vertx.logger-delegate-factory-class-name=org.vertx.java.core.logging.impl.SLF4JLogDelegateFactory "
-JVM_ARGS="$JVM_ARGS -Xss512k -Xms128m -Xmx512m"
+JVM_ARGS="$JVM_ARGS -Xss512k -Xms128m -Xmx256m"
 JVM_ARGS="$JVM_ARGS -cp $CP:$MARU_PATH/war"
 
 nohup $JAVA_PATH/java $JVM_ARGS com.pgmate.pay.main.VertXServerNew > /dev/null 2>&1 &
