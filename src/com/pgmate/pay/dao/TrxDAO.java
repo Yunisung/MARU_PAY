84f6891c1e47309a5b4e01c62aaef64794e9a2fc
yangwenry <yangwenry@bkwinners.com>
2023-03-02 15:41:11 +0900

    가상계좌 해지시 DTL 삭제에서 대기상태로 변경

--- src/com/pgmate/pay/dao/TrxDAO.java
+++ src/com/pgmate/pay/dao/TrxDAO.java
@@ -3203,6 +3203,27 @@ public class TrxDAO extends DAO {
 		
 		return deleted;
 	}
+
+	public boolean updateVactDtlReady(String issueId, String holderName) {
+		super.setTable("PG_VACT_DTL");
+
+		super.setRecord("status", "대기");
+		super.setRecord("amount", 0);
+		super.setRecord("oper", "ge");
+		super.setRecord("trackId", "");
+		super.setRecord("depositCnt", 0);
+		super.setRecord("transferKey", "");
+		super.setRecord("holderName", holderName);
+		super.setRecord("udf1", "");
+		super.setRecord("udf2", "");
+		super.setRecord("regDay", 	CommonUtil.getCurrentDate("yyyyMMdd"));
+		super.addWhere("issueId", 	issueId);
+		boolean update = super.update();
+		logger.info("set PG_VACT_DTL close : {}",update );
+
+		super.initRecord();
+		return update;
+	}
 	
 	/**
 	 * 가상계좌 출금계좌 등록 테이블에 저장 (PG_VACT_REG)
