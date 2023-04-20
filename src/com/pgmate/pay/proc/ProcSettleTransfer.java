73e7852e08ac2a41363609c117ed206edc6dbd3d
pys890102 <pys890102@bkwinners.com>
2023-04-20 17:29:51 +0900

    경남은행 가상계좌 - 출금수수료 로직 변경

--- src/com/pgmate/pay/proc/ProcSettleTransfer.java
+++ src/com/pgmate/pay/proc/ProcSettleTransfer.java
@@ -7,6 +7,8 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CodingErrorAction;
 
+import com.pgmate.pay.bean.Vact;
+import com.pgmate.pay.dao.VactDAO;
 import com.pgmate.pay.util.KSignUtil;
 import com.pgmate.lib.vertx.main.VertXUtil;
 import com.pgmate.pay.bean.Transfer;
@@ -104,6 +106,23 @@ public class ProcSettleTransfer extends Proc {
 		trxMap.put("fee", fee);
 		trxMap.put("feeVat", feeVat);
 		// 모계좌(케이뱅크,케이에스넷) 출금수수료 변경(90원 VAT포함 99원)
+
+		//230420_PYS : 출금수수료 세팅
+		//케이뱅크 : 90
+		//경남은행 : 당행(100), 타행(200)
+		VactDAO vactDAO = new VactDAO();
+		String vactBankCd = vactDAO.getVactBank(mchtId);
+
+		if(vactBankCd.equals("089")) {
+			trxMap.put("bankFee", 99);
+		}else if(vactBankCd.equals("039")) {
+			if(request.transfer.bankCd.equals("039")) {
+				trxMap.put("bankFee", 110);
+			} else {
+				trxMap.put("bankFee", 220);
+			}
+		}
+
 		trxMap.put("bankFee", 99);
 //		if(response.transfer.bankCd.equals("020")) {
 //			trxMap.put("bankFee", 50);
--- src/com/pgmate/pay/proc/ProcSettleTransferAccount.java
+++ src/com/pgmate/pay/proc/ProcSettleTransferAccount.java
@@ -111,13 +111,23 @@ public class ProcSettleTransferAccount extends Proc {
 		trxMap.put("amount", request.transfer.amount);
 		trxMap.put("fee", fee);
 		trxMap.put("feeVat", feeVat);
-		// 모계좌(케이뱅크,케이에스넷) 출금수수료 변경(90원 VAT포함 99원)
-		trxMap.put("bankFee", 99);
-//		if(response.transfer.bankCd.equals("020")) {
-//			trxMap.put("bankFee", 50);
-//		}else {
-//			trxMap.put("bankFee", 100);
-//		}
+
+		//230420_PYS : 출금수수료 세팅
+		//케이뱅크 : 90
+		//경남은행 : 당행(100), 타행(200)
+		VactDAO vactDAO = new VactDAO();
+		String vactBankCd = vactDAO.getVactBank(mchtId);
+
+		if(vactBankCd.equals("089")) {
+			trxMap.put("bankFee", 99);
+		}else if(vactBankCd.equals("039")) {
+			if(request.transfer.bankCd.equals("039")) {
+				trxMap.put("bankFee", 110);
+			} else {
+				trxMap.put("bankFee", 220);
+			}
+		}
+
 		trxMap.put("netAmount", transferNetAmount);
 		trxMap.put("balance", transferBalance);
 		trxMap.put("trackId", request.transfer.trackId);
