5b6bf271c9d852a5f9ed077f322138ffdd67e66b
kjm <jjggooo>
2023-06-21 16:26:20 +0900

    mobile 약관 수정 3차

--- war/static/js/common_mb.js
+++ war/static/js/common_mb.js
@@ -66,16 +66,19 @@ function provisionStyling() {
     }
     $("span.detail").each(function () {
         let anchor = null;
-        // if (KWON.util.isMobile()) {
-        //     anchor = $(document.createElement('a')).prop({
-        //         target: '_blank',
-        //         // target: '#',
-        //         href: "/form/payment/auth/step_01_provision" + i + ".html",
-        //         innerText: '약관보기'
-        //     })
-        // } else {
+        if (KWON.util.isMobile()) {
+            // anchor = $(document.createElement('a')).prop({
+            //     target: '_blank',
+            //     // target: '#',
+            //     href: "/form/payment/auth/step_01_provision" + i + ".html",
+            //     innerText: '약관보기'
+            // })
+            var div = $(document.createElement('div'));
+            anchor = $(div).append($(document.createElement('a')).prop({href: '#', innerText: '약관보기'}));
+
+        } else {
             anchor = $(document.createElement('a')).prop({href: '#', innerText: '약관보기'})
-        // }
+        }
         $(this).append(anchor);
         i = i + 1;
     });
