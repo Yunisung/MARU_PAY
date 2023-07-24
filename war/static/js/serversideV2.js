dd1e61184d3a1e9aed971867a9057bde1740c780
pys890102 <pys890102@bkwinners.com>
2023-07-24 13:57:05 +0900

    230724_갤럭시아 모바일 8차
    결제 모듈 해상도 변경

--- war/static/js/serversideV2.js
+++ war/static/js/serversideV2.js
@@ -284,6 +284,8 @@ function openPayment(config) {
                 document.getElementById('iframe-payment-containner').style.width = ServerUtil.layerWidth;
                 document.getElementById('iframe-payment').src = url;
                 document.getElementById('iframe-payment').style.display = 'block';
+                document.getElementById('iframe-payment').style.height = ServerUtil.layerHeigth;
+                document.getElementById('iframe-payment').style.width = ServerUtil.layerWidth;
             }
         } else {
             /* 정상적이지 않을 경우, 결제가 불가능한 경우이므로 창을 닫고 알람을 띄운다. */
