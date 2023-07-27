dd318fe5651c1fb719c5866535e3fd38690f4d7f
pys890102 <pys890102@bkwinners.com>
2023-07-27 14:18:19 +0900

    230727_코드원복

--- war/static/js/serversideV2.js
+++ war/static/js/serversideV2.js
@@ -228,25 +228,7 @@ function PopupCenter(url, title, w, h) {
     var newWindow = null;
     console.log('Window Open!');
 
-    var f = document.createElement('form')
-    f.setAttribute('method', 'get')
-    f.setAttribute('target', 'thepayone-payment')
-    f.setAttribute('action', url)
-    var i = document.createElement("input"); //input element, text
-    i.setAttribute('type', "text");
-    i.setAttribute('name', "token");
-    i.setAttribute('value', url.split('?token=')[1]);
-    var s = document.createElement("input"); //input element, Submit button
-    s.setAttribute('type', "submit");
-    s.setAttribute('value', "Submit");
-    f.appendChild(i);
-    f.appendChild(s);
-    document.getElementsByTagName('body')[0].appendChild(f);
-    f.style.display = 'none';
-    f.submit();
-
-
-    /*if (util.isMobile()) {
+    if (util.isMobile()) {
         console.log('Mobile Open!');
         newWindow = window.open('about:blank', title);
     } else {
@@ -275,7 +257,7 @@ function PopupCenter(url, title, w, h) {
         if (window.focus) {
             newWindow.focus();
         }
-    }*/
+    }
 }
 
 
