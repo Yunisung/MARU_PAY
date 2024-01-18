4cdd1aa0d8fa0d6b67e626a74fe6b44dc91d5d36
kjm <jjggooo>
2024-01-18 13:33:40 +0900

    수기결제 - 월세앱 한도 체크 중괄호 수정

--- src/com/pgmate/pay/proc/ProcPay.java
+++ src/com/pgmate/pay/proc/ProcPay.java
@@ -517,13 +517,14 @@ public class ProcPay extends Proc {
 						}
 					}
 				}
-			// 월한도
-			if (rentLimitMonth > 0) {
-				long monthSum = trxDAO.getRentMonthSum(CommonUtil.getCurrentDate("yyyyMM"), mchtMap.getString("mchtId"), billingType);
-				if (rentLimitMonth < request.widget.getLong("amount") + monthSum) {
-					logger.debug("가맹점 월 한도초과 : {},{},{}", rentLimitMonth, request.widget.getLong("amount"), monthSum);
-					response.result = ResultUtil.getResult("9999", "한도초과", "가맹점 월 거래한도 초과");
-					return;
+				// 월한도
+				if (rentLimitMonth > 0) {
+					long monthSum = trxDAO.getRentMonthSum(CommonUtil.getCurrentDate("yyyyMM"), mchtMap.getString("mchtId"), billingType);
+					if (rentLimitMonth < request.widget.getLong("amount") + monthSum) {
+						logger.debug("가맹점 월 한도초과 : {},{},{}", rentLimitMonth, request.widget.getLong("amount"), monthSum);
+						response.result = ResultUtil.getResult("9999", "한도초과", "가맹점 월 거래한도 초과");
+						return;
+					}
 				}
 			}
 		}
