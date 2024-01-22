95db068d8574d9e0998885f3b07ab0e311cc77b7
kjm <jjggooo>
2024-01-22 15:31:22 +0900

    [월세앱] redirect alert test 롤백

--- war/static/js/clientsideV2.js
+++ war/static/js/clientsideV2.js
@@ -264,7 +264,6 @@ var MARU = (function (win, doc) {
 	    var url = c3Config.redirectUrl;
 	    if (url && url.length > 1) {
 	      console.log('[CLIENT] REDIRECT: ', url);
-          alert('alert test');
 	      var form = doc.createElement('form');
 	      form.method = 'GET';
 	      form.action = url;
