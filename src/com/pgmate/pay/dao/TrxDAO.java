6ada1efae8c8f8356eb012a6015dc09c6ec519ef
yangwenry <yangwenry@bkwinners.com>
2024-02-02 17:58:11 +0900

    [월세앱] 보증금 한도 체크 쿼리 필드 변경 billingMethod -> billingType 2차

--- src/com/pgmate/pay/dao/TrxDAO.java
+++ src/com/pgmate/pay/dao/TrxDAO.java
@@ -5947,12 +5947,11 @@ public class TrxDAO extends DAO {
 		return rset.getRowFirst().getLong("totAmount");
 	}
 
-	public long getRentBeforeSum(String yearMonth, String mchtId, String billingType) {
+	public long getRentBeforeSum(String mchtId, String billingType) {
 		super.setTable("VW_TRX_CAP");
 		super.setColumns("SUM(amount) as totAmount");
 		super.addWhere("mchtId", mchtId, eq);
 		//super.addWhere("capType", "매입", eq);
-		super.addWhere("substr(trxDay, 1, 6)", yearMonth, eq);
 		super.addWhere("billingType", billingType, eq);
 		super.addWhere("serviceType", "월세앱", eq);
 		RecordSet rset = super.search();
