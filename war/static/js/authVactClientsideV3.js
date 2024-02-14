ec124e2eb9bb2e43dd1bad5b7e87f3a377bf5507
pys890102 <pys890102@bkwinners.com>
2024-02-14 13:47:05 +0900

    240214_레이어팝업 URL변경

--- war/static/js/authVactClientsideV3.js
+++ war/static/js/authVactClientsideV3.js
@@ -66,8 +66,8 @@ var KWON = (function (win, doc) {
         local: 'http://127.0.0.1:10002',
     }
     //부국위너스 URL주소
-    var maruUrl = 'http://127.0.0.1:10002'; //local
-    // var maruUrl = 'https://devapi.bkwinners.kr'; //dev
+    // var maruUrl = 'http://127.0.0.1:10002'; //local
+    var maruUrl = 'https://devapi.bkwinners.kr'; //dev
     // var maruUrl = 'https://api.bkwinners.kr'; //live
 
     var routeDomain = routeUrls[c3Config.debugMode];
