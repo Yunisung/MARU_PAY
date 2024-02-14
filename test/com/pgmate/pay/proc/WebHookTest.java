93a87d90e0f35838640764f6780c8456a193f80b
yangwenry <yangwenry@bkwinners.com>
2024-02-14 16:06:11 +0900

    [테스트코드] 웹훅 테스트 추가

--- test/com/pgmate/pay/proc/WebHookTest.java
+++ test/com/pgmate/pay/proc/WebHookTest.java
@@ -29,7 +29,7 @@ public class WebHookTest {
     }
 
     @Test
-    public void 노티생성_RFD() {
+    public void createNotiForRfd() {
         String rootTrxId = "";
         String webhookUrl = "https://chpayment.co.kr/__payment_log/pg_noti_bukook.php";
 
@@ -49,10 +49,61 @@ public class WebHookTest {
     }
 
     @Test
-    public void 샘플() {
+    public void sample() {
         String account = "123456789123456789";
         int accountLen = account.length();
         String result =  "******"+ account.substring(accountLen-8, accountLen);
         logger.info("result => {}, {}", accountLen, result);
+    }
+
+    @Test
+    public void substringPayload() {
+        String payLoad = "{\n" +
+                "  \"result\": {\n" +
+                "    \"resultCd\": \"0000\",\n" +
+                "    \"resultMsg\": \"정상\",\n" +
+                "    \"advanceMsg\": \"정상승인\",\n" +
+                "    \"create\": \"20240214131126\"\n" +
+                "  },\n" +
+                "  \"pay\": {\n" +
+                "    \"authCd\": \"09787167\",\n" +
+                "    \"card\": {\n" +
+                "      \"cardId\": \"card_3b77-6d5b07-f2a-0702a\",\n" +
+                "      \"installment\": 0,\n" +
+                "      \"bin\": \"451842\",\n" +
+                "      \"last4\": \"346*\",\n" +
+                "      \"issuer\": \"신한\",\n" +
+                "      \"cardType\": \"신용\",\n" +
+                "      \"acquirer\": \"신한\",\n" +
+                "      \"issuerCode\": \"\",\n" +
+                "      \"acquirerCode\": \"\"\n" +
+                "    },\n" +
+                "    \"webhookUrl\": \"https://localhost:3000/\",\n" +
+                "    \"products\": [\n" +
+                "      {\n" +
+                "        \"prodId\": \"\",\n" +
+                "        \"name\": \"test\",\n" +
+                "        \"qty\": 1,\n" +
+                "        \"price\": 1000,\n" +
+                "        \"desc\": \"deq-scription\"\n" +
+                "      }\n" +
+                "    ],\n" +
+                "    \"trxId\": \"T240214544193\",\n" +
+                "    \"trxType\": \"3DTR\",\n" +
+                "    \"tmnId\": \"TMN004924\",\n" +
+                "    \"trackId\": \"202402141310432258494\",\n" +
+                "    \"amount\": 1000,\n" +
+                "    \"udf1\": \"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff\",\n" +
+                "    \"udf2\": \"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff\"\n" +
+                "  }\n" +
+                "}";
+
+        String ntsPayload = "";
+        if(payLoad.length() > 1024) {
+            ntsPayload = payLoad.substring(0, 1024);
+        } else {
+            ntsPayload= payLoad;
+        }
+        logger.info("payLoad size: {}, value: {}", payLoad.length(), ntsPayload);
     }
 }
