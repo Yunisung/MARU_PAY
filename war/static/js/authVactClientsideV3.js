65583bfab97a7f44098fe435e0dc1485691829ce
pys890102 <pys890102@bkwinners.com>
2024-02-23 16:08:11 +0900

    240223_통합인증 로직 리뉴얼

--- war/static/js/authVactClientsideV3.js
+++ war/static/js/authVactClientsideV3.js
@@ -113,7 +113,7 @@ var KWON = (function (win, doc) {
     }
 
     function requestSendVact() {
-        maruPop();
+        //maruPop();
 
         setTimeout(function() {
             postMessages.sendVact();
