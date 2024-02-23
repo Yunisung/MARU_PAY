3c5bc080604e12297bf4cbe387973f0feb2618ce
pys890102 <pys890102@bkwinners.com>
2024-02-23 13:11:34 +0900

    240223_가상계좌 개인계좌 로직 수정

--- src/com/pgmate/pay/proc/ProcVactAuthOpen.java
+++ src/com/pgmate/pay/proc/ProcVactAuthOpen.java
@@ -1040,16 +1040,16 @@ public class ProcVactAuthOpen extends Proc{
         if(mchtVactMngMap.getString("personalAccountCheck").equals("N")) {
             return true;
         } else {
+            String bankCd = request.vact.bankCd;
             String holderName = request.vact.holderName;
             String identity = request.vact.identity;
-            String mchtId = mchtMap.getString("mchtId");
 
             if(identity.equals("")) {
                 response.result = ResultUtil.getResult("9999", "생년월일 미입력","생년월일이 입력되지 않았습니다. 관리자에 문의 바랍니다");
                 return false;
             }
 
-            int accountCnt = trxDAO.getPersonalAccount(holderName, identity, mchtId);
+            int accountCnt = trxDAO.getPersonalAccount(holderName, identity, bankCd);
 
             if(accountCnt > 0) {
                 response.result = ResultUtil.getResult("9999", "사용자 정보 중복","동일 사용자 정보로 발급된 가상계좌가 존재합니다. 관리자에 문의 바랍니다");
