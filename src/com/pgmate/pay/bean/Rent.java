d5955c6bb11fdcdff31fa34977ca0dd3ea5fef84
kjm <jjggooo>
2024-03-13 17:45:06 +0900

    월세앱 - 선납 처리 3차(payMonth 추가)

--- src/com/pgmate/pay/bean/Rent.java
+++ src/com/pgmate/pay/bean/Rent.java
@@ -3,5 +3,5 @@ package com.pgmate.pay.bean;
 public class Rent {
     public String billingMethod		= "";       // 납부구분(일반, 분납, 선납)
     public String billingType		= "";       // 결제유형(월세, 보증금)
-
+    public String payMonth          = "";       // 선납 개월 수
 }
