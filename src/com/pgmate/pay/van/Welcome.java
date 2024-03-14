703213111af9da34aa1675956b9b45e217d46798
kjm <jjggooo>
2024-03-14 17:51:04 +0900

    웰컴 - 서버 롤백

--- src/com/pgmate/pay/van/Welcome.java
+++ src/com/pgmate/pay/van/Welcome.java
@@ -18,10 +18,12 @@ import java.net.*;
 public class Welcome implements Van {
     private static Logger logger 	= LoggerFactory.getLogger( Welcome.class );
 
-//    private static final String cancel_uri		= "https://payapi.paywelcome.co.kr/cancel/cancel";
-//    private static final String approval_uri	= "https://payapi.paywelcome.co.kr/noauth/pay/card";
-    private static final String cancel_uri		= "https://tpayapi.paywelcome.co.kr/cancel/cancel";
-    private static final String approval_uri	= "https://tpayapi.paywelcome.co.kr/noauth/pay/card";
+    // live
+    private static final String cancel_uri		= "https://payapi.paywelcome.co.kr/cancel/cancel";
+    private static final String approval_uri	= "https://payapi.paywelcome.co.kr/noauth/pay/card";
+    //test
+//    private static final String cancel_uri		= "https://tpayapi.paywelcome.co.kr/cancel/cancel";
+//    private static final String approval_uri	= "https://tpayapi.paywelcome.co.kr/noauth/pay/card";
 
     private String mid 					= "";
     private String VAN					= "";
