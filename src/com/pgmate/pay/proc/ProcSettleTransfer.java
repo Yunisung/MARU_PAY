c8dc1ce2e445cd08f142cc8d08e9d9368884f33d
kjm <jjggooo>
2024-08-07 14:26:02 +0900

    충전정산 - 보류금액(비율) 설정 추가

--- src/com/pgmate/pay/proc/ProcSettleTransfer.java
+++ src/com/pgmate/pay/proc/ProcSettleTransfer.java
@@ -70,10 +70,18 @@ public class ProcSettleTransfer extends Proc {
 			nonDeposit = getNotDeposit(mchtId);
 		}
 
+		// 비율금액
+		Calendar cal = Calendar.getInstance();
+		cal.add(cal.DATE, -1);
+		String yesterDay = new SimpleDateFormat("yyyyMMdd").format(cal.getTime());
+		long chargeAmt = trxDAO.getTrxNetAmount(mchtId, yesterDay);
+		long trxAmt = trxDAO.getTrxAmount(mchtId, yesterDay);
+		long transferLimitPersentAmt = (long) ((chargeAmt + trxAmt) * chargeMng.getDouble("transferLimitPercent"));
+
 		long netAmount = request.transfer.amount + fee + feeVat;
-		long checkAmount = netAmount + transferLimit + nonDeposit;
+		long checkAmount = netAmount + transferLimit + nonDeposit + transferLimitPersentAmt;
 		if(checkAmount > balance) {
-			logger.debug("잔액부족 - 현재잔액: {},가맹점계정 차감예정액: {},보류금액:{},미수금액:{}",balance,netAmount,transferLimit,nonDeposit);
+			logger.debug("잔액부족 - 현재잔액: {},가맹점계정 차감예정액: {},보류금액: {},전일기준보류금액: {},미수금액: {}",balance,netAmount,transferLimit,transferLimitPersentAmt,nonDeposit);
 			response.result = ResultUtil.getResult("9999", "잔액부족","잔액이 부족합니다.");
 			sendResponse();
 			return;
