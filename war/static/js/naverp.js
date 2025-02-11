fae94db3cca23c68280e9bb69ae3f8f8c3f92544
pys890102 <pys890102@bkwinners.com>
2025-02-11 14:49:14 +0900

    250211_간편결제 미응답 오류 수정

--- war/static/js/naverp.js
+++ war/static/js/naverp.js
@@ -173,11 +173,11 @@ var C3MOD = (function (win, doc) {
         },
         sendMessageToParent: function (obj) {
             if (window.opener) {
-                console.log('SEND POSTMESSAGE TO OPENER', obj);
+                // console.log('SEND POSTMESSAGE TO OPENER', obj);
                 util.log('SEND POSTMESSAGE TO OPENER', obj);
                 window.opener.postMessage(JSON.stringify(obj), "*");
             } else {
-                console.log('SEND POSTMESSAGE TO PARENT', obj);
+                // console.log('SEND POSTMESSAGE TO PARENT', obj);
                 util.log('SEND POSTMESSAGE TO PARENT', obj);
                 window.parent.postMessage(JSON.stringify(obj), "*");
             }
@@ -255,7 +255,13 @@ var C3MOD = (function (win, doc) {
     util.addEventListener(window, 'message', function (e) {
         util.log(e.source);
         util.log(e.origin);
-        var recv = JSON.parse(e.data);
+        var recv;
+        try{
+            recv = JSON.parse(e.data);
+        } catch(error) {
+            util.log('Invalid JSON received:', e.data);
+            return;
+        }
         util.log(recv);
         if (!recv.type) {} else if (recv.type === 'PAY_CLOSE') {
             util.sendMessageToParent(recv);
@@ -286,7 +292,7 @@ var C3MOD = (function (win, doc) {
         c3_Config = JSON.parse(config).widget;
         c3_Config.form = JSON.parse(c3_Config.form);
 
-        console.log('c3_Config', c3_Config);
+        // console.log('c3_Config', c3_Config);
 
         if(c3_Config.device == 'mobile' && !c3_Config.redirectUrl) {
             alert('결제 오류, 모바일 결제 필수값이 존재하지 않습니다.');
@@ -341,7 +347,7 @@ var C3MOD = (function (win, doc) {
 /* 팝업창에서 부모창으로 데이터 보내는 로직 */
 function kspayToParent() {
     var resObj = document.forms.frm.data.value;
-    console.log('kspayToParent: ', resObj, decodeURIComponent(resObj));
+    // console.log('kspayToParent: ', resObj, decodeURIComponent(resObj));
     //close(true);
     setTimeout(function () {
         var obj = {
@@ -350,10 +356,10 @@ function kspayToParent() {
 
         obj.data = decodeURIComponent(resObj);
         if (window.opener) {
-            console.log('SEND POSTMESSAGE TO OPENER', obj);
+            // console.log('SEND POSTMESSAGE TO OPENER', obj);
             window.opener.postMessage(JSON.stringify(obj), "*");
         } else {
-            console.log('SEND POSTMESSAGE TO PARENT', obj);
+            // console.log('SEND POSTMESSAGE TO PARENT', obj);
             window.parent.postMessage(JSON.stringify(obj), "*");
         }
         setTimeout(function () {
