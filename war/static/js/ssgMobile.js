a84f7fb7ce6f98a466e71caefb5fccbf5fbfa92b
pys890102 <pys890102@bkwinners.com>
2025-02-11 14:49:14 +0900

    250211_간편결제 미응답 오류 수정
    # Conflicts:
    #	war/static/js/kakao.js

--- war/static/js/ssgMobile.js
+++ war/static/js/ssgMobile.js
@@ -173,11 +173,11 @@ var C3MOD = (function (win, doc) {
         },
         sendMessageToParent: function (obj) {
             if (window.opener) {
-                console.log('SEND POSTMESSAGE TO OPENER', obj);
+                // console.log('SEND POSTMESSAGE TO OPENER', obj);
                 util.log('SEND POSTMESSAGE TO OPENER', obj);
                 window.opener.postMessage(JSON.stringify(obj), "*");
             } else {
-                console.log('SEND POSTMESSAGE TO PARENT', obj);
+                // console.log('SEND POSTMESSAGE TO PARENT', obj);
                 util.log('SEND POSTMESSAGE TO PARENT', obj);
                 window.parent.postMessage(JSON.stringify(obj), "*");
             }
@@ -273,7 +273,13 @@ var C3MOD = (function (win, doc) {
     util.addEventListener(window, 'message', function (e) {
         util.log(e.source);
         util.log(e.origin);
-        var recv = JSON.parse(e.data);
+        var recv;
+        try{
+            recv = JSON.parse(e.data);
+        } catch(error) {
+            util.log('Invalid JSON received:', e.data);
+            return;
+        }
         util.log(recv);
         if (!recv.type) {} else if (recv.type === 'PAY_CLOSE') {
             util.sendMessageToParent(recv);
@@ -306,7 +312,7 @@ var C3MOD = (function (win, doc) {
         c3_Config.authForm = JSON.parse(c3_Config.authForm);
 
 //       c3_Config.targetUrl = c3_Config.targetUrl.replace('\u003d', '=');
-        console.log('c3_Config', c3_Config);
+//         console.log('c3_Config', c3_Config);
 
         //간편결제 모바일때만 redirectURL 사용
         if(c3_Config.device == 'mobile' && !c3_Config.redirectUrl) {
@@ -330,9 +336,9 @@ var C3MOD = (function (win, doc) {
         }, true);
 
         var search = window.location.search.split('&');
-        console.log('search: ' + search);
+        // console.log('search: ' + search);
         var token = search[0].split('=')[1];
-        console.log('token : ' + token);
+        // console.log('token : ' + token);
 
         getConfigByToken(token, function (res) {
             if(!validation(res)) {
@@ -362,7 +368,7 @@ var C3MOD = (function (win, doc) {
 /* 팝업창에서 부모창으로 데이터 보내는 로직 */
 function kspayToParent() {
     var resObj = document.forms.frm.data.value;
-    console.log('kspayToParent: ', resObj, decodeURIComponent(resObj));
+    // console.log('kspayToParent: ', resObj, decodeURIComponent(resObj));
     //close(true);
     setTimeout(function () {
         var obj = {
@@ -371,10 +377,10 @@ function kspayToParent() {
 
         obj.data = decodeURIComponent(resObj);
         if (window.opener) {
-            console.log('SEND POSTMESSAGE TO OPENER', obj);
+            // console.log('SEND POSTMESSAGE TO OPENER', obj);
             window.opener.postMessage(JSON.stringify(obj), "*");
         } else {
-            console.log('SEND POSTMESSAGE TO PARENT', obj);
+            // console.log('SEND POSTMESSAGE TO PARENT', obj);
             window.parent.postMessage(JSON.stringify(obj), "*");
         }
         setTimeout(function () {
