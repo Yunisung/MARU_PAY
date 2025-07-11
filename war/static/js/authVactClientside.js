11aedd4dfbb698dadd2727cfd4380d964714b382
pys890102 <pys890102@bkwinners.com>
2025-07-11 14:47:21 +0900

    URL 변경
    # Conflicts:
    #	src/com/pgmate/pay/util/PAYUNIT.java
    #	war/static/js/authNoVactClientside.js
    #	war/static/js/clientside.js
    #	war/static/js/clientsideV2.js
    #	war/static/js/onlyAuthClientside.js

--- war/static/js/authVactClientside.js
+++ war/static/js/authVactClientside.js
@@ -64,8 +64,8 @@ var KWON = (function (win, doc) {
 	var maruUrl = 'https://api.bkwinners.kr'; //live
 
 	var maruUrls = {
-		test: 'https://devapi.bkwinners.kr',
-		live: 'https://api.bkwinners.kr'
+		test: 'https://devapi.ghpayments.kr',
+		live: 'https://api.ghpayments.kr'
 	}
 
 
